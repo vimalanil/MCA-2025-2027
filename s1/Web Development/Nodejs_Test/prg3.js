ad613b2c9944fd0211f9e9fff5198d404de6f0d0
v <vimalanil4442.com>
2025-10-28 11:59:41 +0530

    node

--- s1/Web Development/Nodejs_Test/prg3.js
+++ s1/Web Development/Nodejs_Test/prg3.js
@@ -1,7 +1,7 @@
 const con = require('./conn.js');
  
 // SQL query to insert one record
-const sql = "INSERT INTO t_db.students (rollno, name) VALUES (6, 'Ram')";
+const sql = "INSERT INTO t_db.students (rollno, name) VALUES (7, 'Umesh')";
  
     con.query(sql, (err, result) => {
   if (err) throw err;
